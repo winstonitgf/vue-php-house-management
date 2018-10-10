9b64dcb8a5c7e6c4968b961d9c8934a8148ba18f
winstonitgf <aa71435723@gmail.com>
2018-10-11 00:21:54 +0800

    123

--- house-management-frontend/src/components/Category.vue
+++ house-management-frontend/src/components/Category.vue
@@ -4,15 +4,15 @@
     <md-card md-with-hover v-for="(item, index) in categories" v-bind:index="index" v-bind:key="item.id">
         <md-ripple>
             <md-card-header>
-                <div class="md-title">{{item.FLEX1}}</div>
+                <div class="md-title">{{item.flex1}}</div>
             </md-card-header>
 
             <md-card-content>
-                {{item.FLEX2}}
+                {{item.flex2}}
             </md-card-content>
 
             <md-card-actions>
-                <md-button @click="goRoute(item.FLEX3)">Entry</md-button>
+                <md-button @click="goRoute(item.flex3)">Entry</md-button>
             </md-card-actions>
         </md-ripple>
     </md-card>
@@ -20,37 +20,50 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Category",
   data() {
     return {
       msg: "Choose a business you want start with.",
-      categories: [
-        {
-          ID: 1,
-          FLEX1: "House",
-          FLEX2: "Manage your house information",
-          FLEX3: "/HouseMain"
-        },
-        {
-          ID: 2,
-          FLEX1: "User",
-          FLEX2: "Manager your user profile",
-          FLEX3: "/UserMain"
-        },
-        {
-          ID: 3,
-          FLEX1: "Contract",
-          FLEX2: "Manager your contract detail",
-          FLEX3: "/ContractMain"
-        }
-      ]
+      categories: null
+      // categories: [
+      //   {
+      //     ID: 1,
+      //     FLEX1: "House",
+      //     FLEX2: "Manage your house information",
+      //     FLEX3: "/HouseMain"
+      //   },
+      //   {
+      //     ID: 2,
+      //     FLEX1: "User",
+      //     FLEX2: "Manager your user profile",
+      //     FLEX3: "/UserMain"
+      //   },
+      //   {
+      //     ID: 3,
+      //     FLEX1: "Contract",
+      //     FLEX2: "Manager your contract detail",
+      //     FLEX3: "/ContractMain"
+      //   }
+      // ]
     };
   },
   methods: {
     goRoute(route) {
       this.$router.push(route);
     }
+  },
+  mounted() {
+    axios
+      .get("http://172.16.0.10/house-management-backend/public/api/configs", {
+        headers: {
+          "Access-Control-Allow-Origin": "http://172.16.0.10",
+          "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, OPTIONS",
+          "Access-Control-Max-Age": "86400"
+        }
+      })
+      .then(response => (this.categories = response.data));
   }
 };
 </script>
--- house-management-frontend/src/components/contract/contract-main.vue
+++ house-management-frontend/src/components/contract/contract-main.vue
@@ -1,7 +1,26 @@
 <template>
   <div>
 
-    <h1>{{ msg }}</h1>
+     <md-speed-dial class="md-bottom-right">
+      <md-speed-dial-target style="background-color:red">
+        <md-icon>menu</md-icon>
+      </md-speed-dial-target>
+
+      <md-speed-dial-content>
+        <md-button class="md-icon-button" @click="goRoute('/Category')">
+          <md-icon>home</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button" @click="goRoute('/HouseMain')">
+          <md-icon>domain</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button" @click="goRoute('/UserMain')">
+          <md-icon>person</md-icon>
+        </md-button>
+
+      </md-speed-dial-content>
+    </md-speed-dial>
 
   </div>
 </template>
